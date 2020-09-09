258d405fb5e3f724052674f594f2ff87751f30ff
Jaime Vega <jaime.vega@knab.nl>
2020-09-09 13:30:55 +0200

    Simple repo to reproduce issue Wallaby/Quokka

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+test("renders learn react link", () => {
+  const { getByText, debug } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  console.log("debug() output\n\n");
+  debug() // ?
+
+  console.log("screen.debug() output\n\n");
+  screen.debug(); // ?
 });
